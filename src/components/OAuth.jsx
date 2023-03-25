5b62263a18c1fa9c15f7e519be2a29c77151c444
UZAIR ATHER <uzairathersaeed>
2023-03-26 01:09:03 +0500

    complete the OAuth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,8 +1,36 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { doc,getDoc, serverTimestamp, setDoc } from 'firebase/firestore'
 import {FcGoogle} from 'react-icons/fc'
-
+import { toast } from 'react-toastify'
+import {db} from '../firebase'
+import { useNavigate } from 'react-router'
 const OAuth = () => {
+  const navigate = useNavigate()
+  async function onGoogleClick(){
+    try {
+      const auth = getAuth() 
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user
+
+      //check for the user
+      const docRef = doc(db,"users",user.uid)
+      const docSnap = await getDoc(docRef)//check all the document with the user.uid
+      if(!docSnap.exists()){
+        await setDoc(docRef,{
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp()
+        })
+      }
+      navigate('/')
+    } catch (error) {
+      toast.error("Could not authorize to google")
+    }
+  }
   return (
-    <button className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 rounded uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900   
+    //by default button has submit type 
+    <button type='button' onClick={onGoogleClick} className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 rounded uppercase text-sm font-medium hover:bg-red- 800 active:bg-red-900   
     shadow-md hover:shadow-lg
     active:shadow-lg transition duration-150 ease-in-out
     '>
